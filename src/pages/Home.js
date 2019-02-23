6e430cebde41fd0a22f291c75bad91c19732221a
jromest <jromestiller@gmail.com>
2019-02-23 09:42:52 +0800

    feat: add cards to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import Layout from '../components/Layout';
 import ProductCarousel from '../components/ProductCarousel';
+import ProductCard from '../components/ProductCard';
 
 const Home = props => (
   <Layout location={props.location}>
-    <Container tag="main" role="main" className="pt-4 pb-4">
+    <Container className="py-4" tag="main" role="main">
       <ProductCarousel />
+      <Row className="pt-3">
+        <Col className="py-4" sm={12} md={6} lg={4}>
+          <ProductCard />
+        </Col>
+        <Col className="py-4" sm={12} md={6} lg={4}>
+          <ProductCard />
+        </Col>
+        <Col className="py-4" sm={12} md={6} lg={4}>
+          <ProductCard />
+        </Col>
+        <Col className="py-4" sm={12} md={6} lg={4}>
+          <ProductCard />
+        </Col>
+        <Col className="py-4" sm={12} md={6} lg={4}>
+          <ProductCard />
+        </Col>
+        <Col className="py-4" sm={12} md={6} lg={4}>
+          <ProductCard />
+        </Col>
+      </Row>
     </Container>
   </Layout>
 );
