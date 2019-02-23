770c6ecdcdd916c74ebe94cbc2943e7e42711e18
jromest <jromestiller@gmail.com>
2019-02-23 09:24:20 +0800

    feat: add carousel to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 import Layout from '../components/Layout';
+import ProductCarousel from '../components/ProductCarousel';
 
 const Home = props => (
   <Layout location={props.location}>
-    <main>
-      <h1>Home</h1>
-    </main>
+    <Container tag="main" role="main" className="pt-4 pb-4">
+      <ProductCarousel />
+    </Container>
   </Layout>
 );
 
