c983824c7f26469766cd3b60fdac4c61e0b36684
jromest <jromestiller@gmail.com>
2019-02-24 15:43:27 +0800

    feat: use product context in product list in manage page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Table, Button } from 'reactstrap';
 
-const ProductList = () => (
+const ProductList = props => (
   <Table striped bordered responsive>
     <thead>
       <tr>
@@ -12,54 +12,21 @@ const ProductList = () => (
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td className="w-25">
-          <img
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Card cap"
-          />
-        </td>
-        <td className="w-25">Otto</td>
-        <td className="w-25">@mdo</td>
-        <td className="w-auto">
-          <Button className="mr-3" color="primary">
-            Edit
-          </Button>
-          <Button color="danger">Delete</Button>
-        </td>
-      </tr>
-      <tr>
-        <td className="w-25">
-          <img
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Card cap"
-          />
-        </td>
-        <td className="w-25">Otto</td>
-        <td className="w-25">@mdo</td>
-        <td className="w-auto">
-          <Button className="mr-3" color="primary">
-            Edit
-          </Button>
-          <Button color="danger">Delete</Button>
-        </td>
-      </tr>
-      <tr>
-        <td className="w-25">
-          <img
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Card cap"
-          />
-        </td>
-        <td className="w-25">Otto</td>
-        <td className="w-25">@mdo</td>
-        <td className="w-auto">
-          <Button className="mr-3" color="primary">
-            Edit
-          </Button>
-          <Button color="danger">Delete</Button>
-        </td>
-      </tr>
+      {props.products.map((product, index) => (
+        <tr key={index}>
+          <td className="w-25">
+            <img src={product.imageUrl} alt={product.name} />
+          </td>
+          <td className="w-25">{product.name}</td>
+          <td className="w-25">{product.description}</td>
+          <td className="w-auto">
+            <Button className="mr-3" color="primary">
+              Edit
+            </Button>
+            <Button color="danger">Delete</Button>
+          </td>
+        </tr>
+      ))}
     </tbody>
   </Table>
 );
--- src/pages/Manage.js
+++ src/pages/Manage.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import { ProductContextConsumer } from '../context/ProductContext';
 import Layout from '../components/Layout';
 import ProductForm from '../components/ProductForm';
 import ProductList from '../components/ProductList';
 
 const Manage = props => (
-  <Layout location={props.location}>
-    <Container className="py-4" tag="main" role="main">
-      <h1 className="mb-5">Product Management</h1>
-      <ProductForm />
-      <h2>Products</h2>
-      <ProductList />
-    </Container>
-  </Layout>
+  <ProductContextConsumer>
+    {({ products }) => (
+      <Layout location={props.location}>
+        <Container className="py-4" tag="main" role="main">
+          <h1 className="mb-5">Product Management</h1>
+          <ProductForm />
+          <h2>Products</h2>
+          <ProductList products={products} />
+        </Container>
+      </Layout>
+    )}
+  </ProductContextConsumer>
 );
 
 export default Manage;
