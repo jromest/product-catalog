217a3894f74f39c761fd0ae4776e76367fc6ee3b
jromest <jromestiller@gmail.com>
2019-02-25 18:55:06 +0800

    feat: transfer logic from ProductList to Manage component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,63 +1,40 @@
 import React from 'react';
 import { Table, Button } from 'reactstrap';
-import ProductForm from '../components/ProductForm';
 
-class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      onEditProduct: null,
-    };
+const ProductList = ({ products, onEdit, onDelete }) => (
+  <Table striped bordered responsive>
+    <thead>
+      <tr>
+        <th>Image</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {products.map((product, index) => (
+        <tr key={index}>
+          <td className="w-25">
+            <img src={product.imageUrl} alt={product.name} />
+          </td>
+          <td className="w-25">{product.name}</td>
+          <td className="w-25">{product.description}</td>
+          <td className="w-auto">
+            <Button
+              className="mr-3"
+              color="primary"
+              onClick={() => onEdit(product)}
+            >
+              Edit
+            </Button>
+            <Button color="danger" onClick={() => onDelete(product)}>
+              Delete
+            </Button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
+);
 
-    this.onEdit = this.onEdit.bind(this);
-  }
-
-  onEdit(product) {
-    this.setState({ onEditProduct: product });
-  }
-
-  render() {
-    const { products, onDelete } = this.props;
-    const { onEditProduct } = this.state;
-
-    return (
-      <React.Fragment>
-        <ProductForm products={onEditProduct} />
-        <Table striped bordered responsive>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product, index) => (
-              <tr key={index}>
-                <td className="w-25">
-                  <img src={product.imageUrl} alt={product.name} />
-                </td>
-                <td className="w-25">{product.name}</td>
-                <td className="w-25">{product.description}</td>
-                <td className="w-auto">
-                  <Button
-                    className="mr-3"
-                    color="primary"
-                    onClick={() => this.onEdit(product)}
-                  >
-                    Edit
-                  </Button>
-                  <Button color="danger" onClick={() => onDelete(product)}>
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </React.Fragment>
-    );
-  }
-}
 export default ProductList;
--- src/pages/Manage.js
+++ src/pages/Manage.js
@@ -2,19 +2,44 @@ import React from 'react';
 import { Container } from 'reactstrap';
 import { ProductContextConsumer } from '../context/ProductContext';
 import Layout from '../components/Layout';
+import ProductForm from '../components/ProductForm';
 import ProductList from '../components/ProductList';
 
-const Manage = props => (
-  <ProductContextConsumer>
-    {({ products, actions }) => (
-      <Layout location={props.location}>
-        <Container className="py-4" tag="main" role="main">
-          <h1 className="mb-5">Product Management</h1>
-          <ProductList products={products} onDelete={actions.delete} />
-        </Container>
-      </Layout>
-    )}
-  </ProductContextConsumer>
-);
+class Manage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onEditProduct: null,
+    };
+
+    this.onEdit = this.onEdit.bind(this);
+  }
+
+  onEdit(product) {
+    this.setState({ onEditProduct: product });
+  }
+
+  render() {
+    const { onEditProduct } = this.state;
+
+    return (
+      <ProductContextConsumer>
+        {({ products, actions }) => (
+          <Layout location={this.props.location}>
+            <Container className="py-4" tag="main" role="main">
+              <h1 className="mb-5">Product Management</h1>
+              <ProductForm products={onEditProduct} />
+              <ProductList
+                products={products}
+                onEdit={this.onEdit}
+                onDelete={actions.delete}
+              />
+            </Container>
+          </Layout>
+        )}
+      </ProductContextConsumer>
+    );
+  }
+}
 
 export default Manage;
