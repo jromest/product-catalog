4f3e10cec79930e2b1617296a12c349c5baf9d18
jromest <jromestiller@gmail.com>
2019-02-26 01:10:19 +0800

    feat: sort card in home page by date creation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,18 +9,25 @@ const Home = props => (
   <ProductContextConsumer>
     {({ products }) => (
       <Layout location={props.location}>
-        <Container className="py-4" tag="main" role="main">
-          <ProductCarousel
-            featuredProducts={products.filter(product => product.isFeatured)}
-          />
-          <Row className="pt-3">
-            {products.map(product => (
-              <Col key={product.id} className="py-4" sm={12} md={6} lg={4}>
-                <ProductCard product={product} />
-              </Col>
-            ))}
-          </Row>
-        </Container>
+        {products && (
+          <Container className="py-4" tag="main" role="main">
+            <ProductCarousel
+              featuredProducts={products.filter(product => product.isFeatured)}
+            />
+            <Row className="pt-3">
+              {products
+                .sort(
+                  (first, second) =>
+                    new Date(first.dateCreated) - new Date(second.dateCreated)
+                )
+                .map(product => (
+                  <Col key={product.id} className="py-4" sm={12} md={6} lg={4}>
+                    <ProductCard product={product} />
+                  </Col>
+                ))}
+            </Row>
+          </Container>
+        )}
       </Layout>
     )}
   </ProductContextConsumer>
